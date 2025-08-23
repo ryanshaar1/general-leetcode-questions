dae93b056646514bfc79ce3e050407440b2f110d
John Doe <shaarryan@gmail.com>
2025-08-23 20:24:20 +0300

    initial commit

---
+++ gas_station_134.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        total_gas = sum(gas)
+        total_cost = sum(cost)
+        
+        if total_gas < total_cost:
+            return -1
+        
+        start = 0
+        tank = 0
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                start = i + 1
+                tank = 0
+        
+        return start
+            
